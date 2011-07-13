42334067c2d4e94eb5f7eea7465fc743ad4034bd
Aaron Feng <aaron.feng@gmail.com>
2011-07-13 16:12:05 -0400

    init commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'http://rubygems.org'
+gem 'rails', '~>3.0'
+gem 'apotomo', "~>1.1.0"
+gem 'haml'
+gem 'sqlite3-ruby', :require => 'sqlite3'
+gem 'jquery-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Apotomo::Application.config.secret_token = '83fb415e6b267ffae9bdf5d66b5723393fc86d199179049d09d1ca490b836183110363c01d79b6a45fad9c9bdadb8d1fc1966ed8cd05e3ef2503d4c67a48ccc0'
